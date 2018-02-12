55db08011a83529551833071101799391875b2fa
Audrey Ho <34053177+pengwy3697@users.noreply.github.com>
2018-02-11 23:09:58 -0500

    Add files via upload

--- assignment_1/SetFullException.java
+++ assignment_1/SetFullException.java
@@ -1,4 +1,4 @@
-package a1;
+package cs445.a1;
 
 /**
  * An exception that is thrown when a set operation cannot be completed because
--- assignment_1/SocialClient.java
+++ assignment_1/SocialClient.java
@@ -1,4 +1,4 @@
-package a1;
+package cs445.a1;
 
 import java.util.NoSuchElementException;
 import java.util.Scanner;
